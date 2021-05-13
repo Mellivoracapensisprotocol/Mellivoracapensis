a614e5817e5f834b326ec8834af291e90a2b563e
Mellivoracapensisprotocol <83501261+Mellivoracapensisprotocol@users.noreply.github.com>
2021-05-13 19:20:54 +0800

    Update Mellivoracapensis.sol

--- Mellivoracapensis.sol
+++ Mellivoracapensis.sol
@@ -3,7 +3,7 @@
    #Mellivoracapensis features:
    3% fee auto add to the liquidity pool to locked forever when selling
    2% fee auto distribute to all holders
-   I created a black hole so #Bee token will deflate itself in supply with every transaction
+   I created a black hole so #MCC token will deflate itself in supply with every transaction
    50% Supply is burned at start.
    
 
